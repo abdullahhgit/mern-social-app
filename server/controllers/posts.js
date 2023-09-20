b667541e13437e3860b0e69f67e708ee4dafaf0f
Abdullah Khan <abdullahkhan42069@gmail.com>
2023-09-20 23:21:01 +0500

    added routes for get and create post, added controller function for get and create post on backend and designed the model for post Message

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,1 +1,32 @@
-const express = require('express')
+const postMessageModel = require('../models/postMessage')
+
+const getPosts = async (req, res) => {
+    try {
+        const postMessage = await postMessageModel.find()
+
+        res.status(200).json(postMessage)
+
+    } catch (error) {
+        res.status(409).json({message: error.message})
+    }
+}
+
+const createPosts = async (req, res) => {
+    const post = req.body;
+
+    const newPost = new postMessageModel(post)
+
+    try {
+        await newPost.save()
+
+        res.status(200).json(newPost)
+
+    } catch (error) {
+        res.status(409).json({message: error.message})
+    }
+}
+
+module.exports = {
+    getPosts,
+    createPosts,
+};
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,9 @@
 const express = require('express');
 
 const router = express.Router();
+const { getPosts, createPosts } = require('../controllers/posts');
 
-router.get('/', (req, res) => {
-    res.send("Hello Abdullah");
-});
+router.get('/', getPosts)
+router.post('/', createPosts)
 
 module.exports = router
