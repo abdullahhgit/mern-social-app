038e5604fc1ad7320ea9bfa8f8918d01b1cd8c41
abdullah-khan <abdullah.khan@devsspace.com>
2023-09-20 11:11:29 +0500

    added routes folder with starter code

--- server/index.js
+++ server/index.js
@@ -1,16 +1,19 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import cors from "cors";
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const router = require("./routes/posts");
 
 const app = express();
+
+const CONNECTION_URL = 'mongodb+srv://admin:admin1234@social-media-app-cluste.7kbb4n8.mongodb.net/'
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json({limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
 app.use(cors())
 
-const CONNECTION_URL = 'mongodb+srv://admin:admin1234@social-media-app-cluste.7kbb4n8.mongodb.net/'
-const PORT = process.env.PORT || 5000;
+app.use('/posts', router); 
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true}) // second params are optional to ensure that mongodb connection string parser successfully.
     .then(() => {
